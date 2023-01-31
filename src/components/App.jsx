801fe2b13102e30b37d46982bad1d32cfe8c163a
GolovinD <golovin.dmytro@gmail.com>
2023-01-31 17:03:24 +0200

    add services

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import Loader from './Loader/Loader'
 import Modal from './Modal/Modal'
 import ImageGallery from './ImageGallery/ImageGallery'
 import Button from './Button/Button'
+import pixabayApi from './services/PixabayAPI'
 
 import css from './App.module.css'
 
@@ -35,7 +36,6 @@ class App extends React.Component {
       searchQuery,
       searchData: [],
       page: 1,
-
     });
     // console.log(this.state);
   }
@@ -61,24 +61,15 @@ class App extends React.Component {
     const prevSearch = prevState.searchQuery;
     const prevPage = prevState.page;
     const nextSearch = searchQuery;
+    
     if (prevSearch !== nextSearch
            || page !== prevPage) {
       // console.log('зміна пропів', nextSearch);
       this.setState({
         status: 'pending',
       });
-        fetch(`https://pixabay.com/api/?q=${nextSearch}&page=${page}&key=31277829-041385667a49103701e539b4a&image_type=photo&orientation=horizontal&per_page=12`)
-      .then(response => {
-          
-        if (response.ok) {
-          return response.json();
-        }
-          return Promise.reject(
-            new Error(`Нажаль не знайдено зображень по запиту - ${nextSearch}`)
-          )
-      })
-          
-      .then(data => { 
+      pixabayApi(nextSearch, page)
+        .then(data => { 
         const { hits } = data
         // console.log(data);
         this.setState({
